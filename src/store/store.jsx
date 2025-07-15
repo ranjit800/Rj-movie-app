68af415788f4236215175c34cba064fdf1689d31
Ranjit jana <jranjit367@gmail.com>
2025-07-15 18:41:41 +0530

    Add TvSlice reducer

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import  movieReducer  from './reducers/movieSlice'
-import  tvReducer  from './reducers/TvSlice'
+import  tvReducer  from './reducers/tvSlice'
 import  personReducer  from './reducers/personSlice'
 
 export default configureStore({
